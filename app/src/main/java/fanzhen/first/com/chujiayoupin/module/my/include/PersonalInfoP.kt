a85f47956e8d55dadfbd682ba6caf64ba5c6dca8
lizongjing <815110332@qq.com>
2018-11-28 10:28:44 +0800

    测试

---
+++ app/src/main/java/fanzhen/first/com/chujiayoupin/module/my/include/PersonalInfoP.kt
@@ -0,0 +1,9 @@
+package fanzhen.first.com.chujiayoupin.module.my.include
+
+import fanzhen.first.com.chujiayoupin.module.my.ui.PersonalIinfoActivity
+/**
+ * Created by li on 2018/8/13.
+ */
+inline fun PersonalIinfoActivity.initBottom(){
+
+}
--- app/src/main/java/fanzhen/first/com/chujiayoupin/module/my/ui/PersonalInfoActivity.kt
+++ app/src/main/java/fanzhen/first/com/chujiayoupin/module/my/ui/PersonalInfoActivity.kt
@@ -1,7 +1,9 @@
 package fanzhen.first.com.chujiayoupin.module.my.ui
 
+import com.dyl.base_lib.img.loadAutoImage
 import fanzhen.first.com.chujiayoupin.R
 import fanzhen.first.com.chujiayoupin.external.BaseActivity
+import kotlinx.android.synthetic.main.activity_personal_info.*
 
 class PersonalIinfoActivity : BaseActivity(){
     override fun initData() {
@@ -11,6 +13,7 @@ class PersonalIinfoActivity : BaseActivity(){
     override fun initView() {
        setContentView(R.layout.activity_personal_info)
         initTitle("个人信息")
+        iv_head_pic.loadAutoImage("", error = R.mipmap.ic_default_head, radius = -1)
     }
 
 }
