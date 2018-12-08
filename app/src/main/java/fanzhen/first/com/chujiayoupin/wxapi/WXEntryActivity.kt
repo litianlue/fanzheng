c5b14f8741ee9e45fa911a1e1d59f12e00f617a9
3ad_100003189134_litianlue <3ad_100003189134_litianlue@git.cloud.tencent.com>
2018-12-08 11:47:10 +0800

    1129

---
+++ app/src/main/java/fanzhen/first/com/chujiayoupin/wxapi/WXEntryActivity.kt
@@ -0,0 +1,7 @@
+package fanzhen.first.com.chujiayoupin.wxapi
+
+import android.app.Activity
+import com.dyl.base_lib.wx.BaseWXEntry
+
+class WXEntryActivity: BaseWXEntry() {
+}
---
+++ app/src/main/java/fanzhen/first/com/chujiayoupin/wxapi/WXPayEntryActivity.kt
@@ -0,0 +1,6 @@
+package fanzhen.first.com.chujiayoupin.wxapi
+
+import com.dyl.base_lib.wx.BaseWXPayEntry
+
+class WXPayEntryActivity: BaseWXPayEntry() {
+}
